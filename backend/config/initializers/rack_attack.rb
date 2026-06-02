68f9394a73041ed7c27549669d2aad8b06de680c
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-06-02 21:53:45 +0200

    Fix Rack::Attack configuration for production

--- backend/config/initializers/rack_attack.rb
+++ backend/config/initializers/rack_attack.rb
@@ -1,22 +1,13 @@
-class Rack::Attack
-#This measure limits login attempts - OWASP A07 Brute Force Protection
-throttle('logins/ip', limit: 5, period: 60.seconds) do |req|
-    if req.path == '/users/login' && req.post?
-        req.ip
-    end
+Rack::Attack.throttle('logins/ip', limit: 5, period: 60) do |req|
+  req.ip if req.path == '/users/login' && req.post?
 end
 
-#Limit register attempts per IP:
-throttle('registrations/ip', limit: 10, period: 60.seconds) do |req|
-    if req.path == 'users/register' && req.post?
-        req.ip
-    end
+Rack::Attack.throttle('registrations/ip', limit: 10, period: 60) do |req|
+  req.ip if req.path == '/users/register' && req.post?
 end
 
-#Block suspicious requests:
-blocklist('block bad actors') do |req|
-    Rack::Attack::Allow2Ban.filter(req.ip, maxretry:10, findtime: 1.minute, bantime: 1.hour) do
-        req.path == '/users/login' && req.post?
-      end
-    end
+Rack::Attack.blocklist('block bad actors') do |req|
+  Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 10, findtime: 60, bantime: 3600) do
+    req.path == '/users/login' && req.post?
+  end
 end
